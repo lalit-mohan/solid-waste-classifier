20030a7ce730f074f1aea72c8c134dcd1e4468ea
keshav sharma <keshavoct98@gmail.com>
2018-10-14 15:17:13 +0530

    Update GUI2.py

--- GUI2.py
+++ GUI2.py
@@ -3,16 +3,13 @@ from Classify import main
 import numpy as np
 import pandas as pd
 from scipy import misc, ndimage
-from tkinter import *
-import tkinter.messagebox
-root = Tk()      
-canvas = Canvas(root, width = 700, height = 550,bg="black")      
-canvas.pack()   
-cam = cv2.VideoCapture(0)
 
-cv2.namedWindow("test")
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (250,250)
+fontScale              = 1
+fontColor              = (255,0,0)
+lineType               = 2
 
-img_counter = 0
 def recycle(img2):
     clf = main()
     dataList2 = []
@@ -25,31 +22,19 @@ def recycle(img2):
     hist = hist.reshape(1,hist.shape[0])
     arr = np.array(hist)
     k = clf.predict(arr)
-    tkinter.messagebox.showinfo( "Result",k)
+    img = cv2.putText(img2, k[0], bottomLeftCornerOfText, font, fontScale, fontColor, lineType)
+    return img
 
+video_capture = cv2.VideoCapture(0)
 while True:
-    ret, frame = cam.read()
-    cv2.imshow("test", frame)
-    if not ret:
+    # Capturing video
+    _, frame = video_capture.read()
+    # Passing individual frames of video
+    canvas = classify(frame)
+    # Showing frame returned from classify function
+    cv2.imshow('Video', canvas)
+    # Press q to exit webcam
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    k = cv2.waitKey(1)
-
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
-        img = PhotoImage(file=img_name)      
-        canvas.create_image(30,30, anchor=NW, image=img)
-        check=Button(root,text="TEST",command=recycle(img_name),background="black",fg="white")
-        check.pack()
-        root.mainloop()    
-
-cam.release()
-
+video_capture.release()
 cv2.destroyAllWindows()
